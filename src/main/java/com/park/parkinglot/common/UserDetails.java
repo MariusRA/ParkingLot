3de621ef9db86c2fa46811dfe8ef056f30962447
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-11-09 11:32:55 +0200

    created a users entry in the menu and successfully displayed data from the database

--- src/main/java/com/park/parkinglot/common/UserDetails.java
+++ src/main/java/com/park/parkinglot/common/UserDetails.java
@@ -11,4 +11,47 @@ package com.park.parkinglot.common;
  */
 public class UserDetails {
     
+    private Integer id;
+    private String username;
+    private String email;
+    private String position;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public UserDetails(Integer id, String username, String email, String position) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.position = position;
+    }
 }
